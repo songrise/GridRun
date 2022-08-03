a4914062bd5d3b291b38e1a33c930e11939b5a39
songrise <1138561926@qq.com>
2022-08-03 17:44:43 +0800

    updated api

--- src/grid_run/runner.py
+++ src/grid_run/runner.py
@@ -1,12 +1,11 @@
 # -*- coding : utf-8 -*-
 # @FileName  : run_exp.py
 # @Author    : Ruixiang JIANG (Songrise)
-# @Time      : Jul 25, 2022
 # @Github    : https://github.com/songrise
-# @Description: Run sh experiments in parallel
+# @Description: Flyweight tool to manage shell commands.
 
 import time
-from typing import List
+from typing import List, Optional
 import numpy as np
 import os
 import datetime
@@ -50,7 +49,7 @@ class Runner():
 
             self.main_log = os.path.join(self.log_root, "main.txt")
 
-    def run(self, exp_names: List[str], instructions: List[str], gpus: List[int] = None, interval_time: int = 0, **kwargs: dict):
+    def run(self, exp_names: List[str], instructions: List[str], gpus: List[int] = None, interval_time: int = 0, **kwargs: dict) -> None:
         """
         execute experiments in parallel, save log to log_name
 
@@ -73,20 +72,20 @@ class Runner():
             exp_names = [f"exp_{i}" for i in range(len(instructions))]
 
         if exp_names is None:
-            self.log("No experiment name specified, use default name")
+            self.log("No experiment name specified, use default name!")
             exp_names = [f"exp_{i}" for i in range(len(instructions))]
 
         self.log("Starting experiments with gpus: {}".format(gpus))
         self.log("Experiments instruction: {}".format(instructions))
+
         # excecute experiments in parallel
         if gpus is None:
-            self.log("No gpus specified, using CUDA 0 by default")
+            self.log("No gpus specified, using CUDA_0 by default!")
             gpus = [0]
 
         num_gpu = len(gpus)
 
         for i, ins in enumerate(instructions):
-            # check if gpu is available
             if i != 0 and i % num_gpu == 0:
                 self.log(
                     "Current running experiment is {}/{}".format(i, len(instructions)))
@@ -94,26 +93,33 @@ class Runner():
                 self.log(
                     "Waiting {} minutes for next set of experiments".format(interval_time))
                 time.sleep(interval_time*60)
+                self.log(
+                    "{} minutes past, attempt to start new set of experiments".format(interval_time))
 
             self.log("Experiment {}: {}".format(i, ins))
             redirect = "> {}_gpu{}.out".format(
                 self.log_root+"/"+exp_names[i], gpus[i % num_gpu])
+
             os.system("CUDA_VISIBLE_DEVICES={} {} {} &".format(
                 gpus[i % num_gpu], instructions[i], redirect))
 
         # get process id
         os.system(f"ps >> {self.main_log}")
 
-    def gen_instruction(self, template, args: List[List], dump_param: bool = True, suffix: str = None) -> list:
+    def compose(self, template: str, args: List[List], dump_param: Optional[bool] = True, suffix: Optional[str] = None) -> List[str]:
         """
         Generate a list of instruction from template and args.
 
         Parameters:
         ----------
-        template : str, the template of instruction, specify the blank as {}.
+        template : str, the template of instruction in the form of format string, specify the blank as {}.
         args : List[List], the list of arguments for each instruction, must much the number of {} in template.
-        dump_param : bool, if True, dump the args and template to a file.
+        dump_param : bool, Optional. if True, dump the args and template to a file.
         suffix : str, the suffix for the dumped file.
+
+        Returns:
+        ----------
+        instructions : list, the list of instructions that are ready to run.
         """
         # log template and args
         if dump_param:
@@ -133,9 +139,9 @@ class Runner():
         args = args.tolist()
         return [template.format(*a) for a in args]
 
-    def log(self, content: str):
+    def log(self, content: str) -> None:
         """
-        Write log to main log file.
+        Write log to the main log file.
         """
         if self.log_main:
             with open(self.main_log, 'a') as f:
@@ -145,13 +151,20 @@ class Runner():
                 f.close()
         return
 
-    def load(self, file_dir: str) -> str:
+    def load(self, file_dir: str):
         """
-        Load a file from log folder.
+        Load dumped args or templates from file.
         Parameters:
         ----------
-        file_dir: str, the path to the file.
+        file_dir: str, the path to the json file.
+
+        Returns:
+        ----------
+        content: list or str, the content of the file.
         """
         with open(os.path.join(self.log_root, file_dir), 'r') as f:
             content = json.load(f)
         return content
+
+    def __str__(self) -> str:
+        return "Runner at: {}".format(self.log_root)
